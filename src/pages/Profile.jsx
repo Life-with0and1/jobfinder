a60cfba8562b6a900ff2353d96d894cf736fa338
Ayush Kumar <ayuuayuu0se1@gmail.com>
2025-06-29 20:00:20 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -48,7 +48,7 @@ function Profile() {
     e.preventDefault();
     try {
       const res = await fetch(
-        `http://localhost:3000/api/user/update/${user._id}`,
+        `https://jobfinder-backend-l5ywfhr45-ayushkumar013s-projects.vercel.app/api/user/update/${user._id}`,
         {
           method: "PATCH",
           headers: {
